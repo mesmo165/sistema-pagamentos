939f0e0af80dea9d80efa8fe54315b96aa0b9507
mesmo165 <eum28406@gmail.com>
2026-08-03 23:24:56 -0500

    Corrige cor dos inputs no iPhone

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
