9a576437621b45387c9e80218eadfd996086a216
mesmo165 <eum28406@gmail.com>
2026-08-04 08:04:07 -0500

    Corrige cor dos inputs no iPhone

--- app/cliente/page.tsx
+++ app/cliente/page.tsx
@@ -141,19 +141,31 @@ if (linkDesativado) {
     margin: "-30px -30px 30px -30px"
   }}
 >
-  <h1 style={{ margin: 0, fontSize: 30 }}>
-    {nomeEmpresa}
-  </h1>
+  <h1
+  style={{
+    margin: 0,
+    fontSize: 42,
+    fontWeight: 800,
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+    textShadow: "0 4px 12px rgba(0,0,0,0.35)",
+    fontFamily: "Poppins, Arial, sans-serif",
+  }}
+>
+  {nomeEmpresa}
+</h1>
 
   <p
-    style={{
-      marginTop: 8,
-      opacity: 0.95,
-      fontSize: 16
-    }}
-  >
-    Plataforma Oficial de Pagamentos
-  </p>
+     style={{
+    marginTop: "10px",
+    color: "rgba(255,255,255,0.95)",
+    fontSize: "17px",
+    fontWeight: 500,
+    letterSpacing: "1px",
+  }}
+>
+  Plataforma Oficial de Pagamentos
+</p>
 </div>
 
         <div style={{ textAlign: "center" }}>
